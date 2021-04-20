c6cdb286fdf43935f3e052a28d8d05cdec5a3003
Yuliia1112 <82772628+Yuliia1112@users.noreply.github.com>
2021-04-20 21:04:31 +0200

    Add files via upload

---
+++ lesson1_task1_Yuliia_Loba.py
@@ -0,0 +1,11 @@
+duration  = int(input("Write the time in seconds:" ))
+
+#if duration // 86400:
+day = duration // 86400
+
+#if duration % 86400 > 3600:
+
+hours = duration // 3600
+min = duration  // 60
+sec = duration % 86400 % 3600 % 60
+print(day , hours, min, sec)
